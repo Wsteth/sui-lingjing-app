85f70377c2e80edd315a8ce8bd0b45c65f7f2517
Xingchen <wsteth@outlook.com>
2024-05-14 22:06:29 +0800

    chore: basic configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,60 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+    "plugin:react/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.tsx"],
+      extends: ["plugin:react-hooks/recommended"],
+    },
+  ],
+  ignorePatterns: [
+    "dist",
+    ".eslintrc.cjs",
+    "postcss.config.cjs",
+    "tailwind.config.js",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "tailwindcss", "unused-imports"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/no-explicit-any": ["warn"],
+    "no-unused-vars": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
   },
-}
+};
